10db24bb7da2f458ead995f2754f551ea9b1335e
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-22 14:10:21 +0100

    feat: create list to menu

---
+++ lib/menu/menu_items.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem(
+      {required this.title,
+      required this.subTitle,
+      required this.link,
+      required this.icon});
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: 'Butões',
+    subTitle: 'Varioes butoes em flutter',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined,
+  ),
+  MenuItem(
+    title: 'Tarjetas',
+    subTitle: 'Varioes butoes em flutter',
+    link: '/card',
+    icon: Icons.credit_card,
+  )
+];
