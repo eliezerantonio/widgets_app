d0101f9bb608ec23b5553c170294e14265807e2c
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-22 20:51:49 +0100

    feat: creations router with GoRouter

---
+++ lib/router/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentations/screens/screens.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/buttons',
+      builder: (context, state) => const ButtonsScreen(),
+    ),
+    GoRoute(
+      path: '/cards',
+      builder: (context, state) => const CardsScreen(),
+    ),
+  ],
+);
