d27eaea7b0a5f3d1ee4c335b19f601b8be0a1ce4
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-22 20:50:19 +0100

    feat: create CardsSreen

---
+++ lib/presentations/screens/cards/cards_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardsScreen extends StatelessWidget {
+  const CardsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Butttons Screen'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
