ede94cfaee23c207b205b5725db2f679cea42ec7
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-23 09:32:13 +0100

    feat: Added more routes

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,54 @@
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/screens/screens.dart';
+
+// GoRouter configuration
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/buttons',
+      name: ButtonsScreen.name,
+      builder: (context, state) => const ButtonsScreen(),
+    ),
+    GoRoute(
+      path: '/cards',
+      name: CardsScreen.name,
+      builder: (context, state) => const CardsScreen(),
+    ),
+    GoRoute(
+      path: '/progress',
+      name: ProgressScreen.name,
+      builder: (context, state) => const ProgressScreen(),
+    ),
+    GoRoute(
+      path: '/snackbars',
+      name: SnackbarScreen.name,
+      builder: (context, state) => const SnackbarScreen(),
+    ),
+    GoRoute(
+      path: '/animated',
+      name: AnimatedScreen.name,
+      builder: (context, state) => const AnimatedScreen(),
+    ),
+    GoRoute(
+      path: '/ui-controls',
+      name: UiControlsScreen.name,
+      builder: (context, state) => const UiControlsScreen(),
+    ),
+    GoRoute(
+      path: '/tutorial',
+      name: AppTutorialScreen.name,
+      builder: (context, state) => const AppTutorialScreen(),
+    ),
+    GoRoute(
+      path: '/infinite',
+      name: InfiniteScrollScreen.name,
+      builder: (context, state) => const InfiniteScrollScreen(),
+    ),
+  ],
+);
